4b215ddd5dc080cb8aed0785663846ed9fc8dde8
kosta2222 <muh_kosta@mail.ru>
2016-11-06 12:31:12 +0300

    init project

---
+++ snake.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tascks;
+namespace Program{
+
+static void Main(string[] args){
+Console.WriteLine("Hello World");
+
+Console.ReadLine();
+}
+}
